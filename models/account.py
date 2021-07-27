0d3eec514ca23dc033da11dd30e7aaf72b068a75
rocketgithub <rodrigo@solucionesprisma.com>
2021-07-27 10:22:17 -0600

    Logging para teashop

--- models/account.py
+++ models/account.py
@@ -12,6 +12,31 @@ import base64
 import logging
 import zeep
 
+import logging.config
+
+logging.config.dictConfig({
+    'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(name)s: %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'zeep.transports': {
+            'level': 'DEBUG',
+            'propagate': True,
+            'handlers': ['console'],
+        },
+    }
+})
+
 class AccountMove(models.Model):
     _inherit = "account.move"
 
