0180fad381e07731676a82c8cb03ba490a30137f
arianaa24 <arianacarrillo24@gmail.com>
2023-09-04 21:44:03 +0000

    Botón en contactos para obtener nombre de facturación fel

--- models/res_partner.py
+++ models/res_partner.py
@@ -6,20 +6,16 @@ import logging
 
 class Partner(models.Model):
     _inherit = "res.partner"
-    
-    @api.onchange("vat")
-    def _onchange_vat(self):
-        self.nit_facturacion_fel = self.vat
-
-    @api.onchange("nit_facturacion_fel")
-    def _onchange_nit_facturacion_fel(self):
-        self.obtener_nombre_facturacion_fel(self.nit_facturacion_fel)
-
-    def obtener_nombre_facturacion_fel(self, nit):
-        client = zeep.Client(wsdl='http://fel.g4sdocumenta.com/ConsultaNIT/ConsultaNIT.asmx?wsdl')
-        resultado = client.service.getNIT(nit, self.env.company.vat, self.env.company.requestor_fel)
-        logging.warning(str(resultado))
-        if resultado['Response']['Result']:
-            self.nombre_facturacion_fel = resultado['Response']['nombre']
-        else:
-            self.nombre_facturacion_fel = 'NIT no valido'
+            
+    def obtener_nombre_facturacion_fel(self):
+        nit = self.nit_facturacion_fel
+        if not nit:
+            nit = self.vat
+        if nit:
+            client = zeep.Client(wsdl='http://fel.g4sdocumenta.com/ConsultaNIT/ConsultaNIT.asmx?wsdl')
+            resultado = client.service.getNIT(nit, self.env.company.vat, self.env.company.requestor_fel)
+            logging.warning(str(resultado))
+            if resultado['Response']['Result']:
+                self.nombre_facturacion_fel = resultado['Response']['nombre']
+            else:
+                self.nombre_facturacion_fel = 'NIT no valido'
