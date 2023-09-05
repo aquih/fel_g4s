dac3ca86a0716e9f7c6b5324650448f8af7f6adf
rocketgithub <rodrigo@solucionesprisma.com>
2023-09-05 17:45:32 -0600

    Validar al obtener datos, no el el botón

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
 import zeep
 import logging
 
@@ -8,21 +9,22 @@ class Partner(models.Model):
     _inherit = "res.partner"
             
     def obtener_nombre_facturacion_fel(self):
-        nit = self.nit_facturacion_fel
-        if not nit:
-            nit = self.vat
-        if nit:
-            res = self._datos_sat(self.env.company, nit)
-            if res['Result'] == True:
-                self.nombre_facturacion_fel = res['nombre']
-            else:
-                self.nombre_facturacion_fel = 'Nit no válido'
+        vat = self.vat
+        if self.nit_facturacion_fel:
+            vat = self.nit_facturacion_fel
+            
+        res = self._datos_sat(self.env.company, vat)
+        self.nombre_facturacion_fel = res['nombre']
                 
     def _datos_sat(self, company, vat):
         if vat:
             client = zeep.Client(wsdl='http://fel.g4sdocumenta.com/ConsultaNIT/ConsultaNIT.asmx?wsdl')
             resultado = client.service.getNIT(vat, company.vat, company.requestor_fel)['Response']
-            resultado['nit'] = resultado['NIT']
             logging.warning(resultado)
-            return resultado
+            
+            if resultado['Result'] == True:
+                return {'nombre': resultado['nombre'], 'nit': resultado['NIT']}
+            else:
+                raise ValidationError(res['error'])
+                
         return {'nombre': '', 'nit': ''}
